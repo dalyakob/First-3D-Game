79b7b2387b89c8375484503413e4085d52963d2e
David Al-Yakobi <dalyakob@umich.edu>
2023-06-18 12:40:48 -0400

    Starter game - First Commit
    Added player / Added player movement / Added Obstacles / Added No Friction Mat to ground floor, and other simple changes.

---
+++ Beefy3DGame/Assets/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody playerMovement; 
+    [SerializeField] float forwardForce = 1000f; 
+    [SerializeField] float sideWaysForce = 1000f;
+    public float jumpForce = 5000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void FixedUpdate()
+    { 
+        playerMovement.AddForce(0,0,forwardForce * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Move Left");
+            playerMovement.AddForce(-sideWaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Move Right");
+            playerMovement.AddForce(sideWaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.Space) && gameObject.transform.position.y <= 1.1f)
+        {
+            Debug.Log("Move up");
+            playerMovement.AddForce(0, jumpForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+        //if (moveLeft)
+        //{
+        //    playerMovement.AddForce(-300, 0, 0);
+        //}
+        //if (moveLeft)
+        //{
+        //    playerMovement.AddForce(-300, 0, 0);
+        //}
+    }
+}
